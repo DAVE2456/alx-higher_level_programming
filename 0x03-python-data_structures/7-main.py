f111a5c749f66fa6a4ff3187ab453314c322cb26
DAVE2456 <davehunegnaw@gmail.com>
2022-08-27 00:05:15 -0400

    7-main.py

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
