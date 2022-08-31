ff703bc6705227c67ee4ce58106990373710aa32
DAVE2456 <davehunegnaw@gmail.com>
2022-08-31 01:57:40 -0400

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print('{:d} is positive'.format(number))
-elif number == 0:
-    print('{:d} is zero'.format(number))
-else:
-    print('{:d} is negative'.format(number))
+def square_matrix_simple(matrix=[]):
+    return [[i ** 2 for i in row] for row in matrix]
