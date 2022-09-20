723fef7a54ab4b77e2fe18cbab967f97e20f4840
DAVE2456 <davehunegnaw@gmail.com>
2022-09-20 03:15:03 -0400

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This is a module that containts a clas that avoids
-dynmaically created attributes
+    101-locked_class: class LockedClass
 """
 
 
 class LockedClass:
+    """
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
     __slots__ = ['first_name']
-
-    def __init__(self):
-        """ Init method """
-        pass
