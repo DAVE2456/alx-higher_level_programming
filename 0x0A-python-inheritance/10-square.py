86afb07a1a1e7387fd734133c85a7015b091fc3c
DAVE2456 <davehunegnaw@gmail.com>
2022-09-26 07:08:20 -0400

    initial commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,8 +10,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-
-    def __str__(self):
-        """Prints the description of the `Rectangle`"""
-        string = "[Square] {}/{}".format(self.__size, self.__size)
-        return 
