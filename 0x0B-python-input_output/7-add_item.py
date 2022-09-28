e86241dd8bc01f2a3ca27ecdf81d8d83668676d9
DAVE2456 <davehunegnaw@gmail.com>
2022-09-28 02:53:39 -0400

    initial commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Handles file input and saves to file"""
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+""" Module that writes an Object to a text file using
+a JSON representation
+"""
+import json
 
 
-argv.pop(0)
-try:
-    deserialized = load_from_json_file("add_item.json")
-    if deserialized is None:
-        save_to_json_file(argv, "add_item.json")
-    else:
-        deserialized.extend(argv)
-        save_to_json_file(deserialized, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(argv, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """ Function that writes an object to a text file
+    by a JSON representation
+    Args:
+        my_obj: object
+        filename: textfile name
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
