b0ed42c3264fb5b3fa97894c2be376c8d70c3d93
DAVE2456 <davehunegnaw@gmail.com>
2022-11-29 14:30:25 +0300

    initial commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is coll");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is coll');
+console.log('JavaScript is amazing');
