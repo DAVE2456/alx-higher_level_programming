578a50a31b8a2a611f8c06365f7d0efbc04a4ea0
DAVE2456 <davehunegnaw@gmail.com>
2022-12-05 13:43:17 +0300

    initial commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-const argvLength = argv.length -2;
+const argvLength = argv.length - 2;
 if (argvLength <= 0) {
     console.log('No argument');
 } else if (argvLength === 1) {
