2cad9b4595f8ee4abf182644f65407f88e8267db
DAVE2456 <davehunegnaw@gmail.com>
2023-01-17 14:07:21 +0300

    initial commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http//swapi.co/apt/films/' + process.argv[2];
-request(url, function (erro, response, body) {
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
