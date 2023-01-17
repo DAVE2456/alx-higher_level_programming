31f2c343362dead421055a27934f605635e3c170
DAVE2456 <davehunegnaw@gmail.com>
2023-01-17 14:38:33 +0300

    initial commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
+/**
+ * Script that prints the title of a Star Wars movie
+ * where the episode number matches a given integer
+ */
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const process = require('process');
+
+const episodNumber = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${episodNumber}`;
+
+request.get(url, (err, _resp, body) => {
+    if (err === null) {
+        const data = JSON.parse(body);
+        console.log(data.title);
+    } else {
+        console.log(err);
+    }
 });
