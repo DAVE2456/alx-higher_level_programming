8596e90fbbd0cece459d73590264312161def28f
DAVE2456 <davehunegnaw@gmail.com>
2023-01-17 13:30:43 +0300

    initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-/**
- * Script that reads from a file
- */
-const process = require('process');
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
